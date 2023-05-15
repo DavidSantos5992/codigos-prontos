22a0a418b8864a74a336c12552b750a0879fab7e
David Santos <davidhenriquehy@gmail.com>
2023-05-15 16:03:02 -0300

    novo esqueleto com tailWind

---
+++ esqueleto/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,css,vue}" // Adicione os tipos de arquivos que você está usando
+  ],
+  theme: {
+    fontFamily: {
+      'Montserrat': ['Montserrat', 'Montserrat'],
+      'MontserratB': ['MontserratB', 'MontserratB'],
+      'MontserratX': ['MontserratX', 'MontserratX'],
+      'Act_Of_Rejection': ['Act_Of_Rejection', 'Act_Of_Rejection'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
