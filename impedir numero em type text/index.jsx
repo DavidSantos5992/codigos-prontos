7e848b8ca470578bd154315360b94a3461d8461c
David <davidhenriquehy@gmail.com>
2023-09-05 13:46:23 -0300

    update

---
+++ impedir numero em type text/index.jsx
@@ -0,0 +1,19 @@
+const isValidInput = (value) => {
+    const onlyLetters = /^[a-zA-Z]*$/; // Expressão regular para aceitar apenas letras
+    return onlyLetters.test(value);
+};
+
+<input type="text"
+id="nameInput"
+
+onChange={(e) => {
+
+    const newValue = e.target.value;
+    if (isValidInput(newValue)) {
+
+    } else {
+        document.getElementById('nameInput').value = ''
+    }
+
+}}
+/>
