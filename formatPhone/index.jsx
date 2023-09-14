c2f9934d8791fc7421e12fc16daeef1a09a7e5e6
David <davidhenriquehy@gmail.com>
2023-09-14 14:38:20 -0300

    update

--- formatPhone/index.jsx
+++ formatPhone/index.jsx
@@ -1,11 +1,31 @@
 const formatPhoneNumber = (phoneNumber) => {
-    const cleanedNumber = phoneNumber.replace(/\D/g, '');
+         
+        
+    if ( phoneNumber.length > 15) {
 
-    let formattedNumber = `(${cleanedNumber.substring(0, 2)}) ${cleanedNumber.substring(2, 7)}-${cleanedNumber.substring(7, 11)}`;
-
-    if (formattedNumber.length == 4) {
-        formattedNumber = ''
+        return phoneNumber = phoneNumber.slice(0, -1) 
+       
     }
 
-    return formattedNumber;
+    console.log(phoneNumber)
+    console.log(phoneNumber.length)
+    const cleanedNumber = phoneNumber.replace(/\D/g, '');
+
+    if (cleanedNumber.length <= 2) {
+        // Se o número for igual ou menor que 2 dígitos, não formate ainda
+        return cleanedNumber;
+    } else if (cleanedNumber.length <= 11) {
+        // Formate como (XX) XXXXX-XXXX
+        let formattedNumber = `(${cleanedNumber.substring(0, 2)}) ${cleanedNumber.substring(2, 7)}`;
+
+        if (cleanedNumber.length >= 8) {
+            // Adicione o hífen e os últimos 4 dígitos se houver
+            formattedNumber += `-${cleanedNumber.substring(7, 11)}`;
+        }
+
+        return formattedNumber;
+    } else {
+        // Se tiver mais de 11 dígitos, apenas pegue os primeiros 11
+        return cleanedNumber.substring(0, 11);
+    }
 }
---
+++ obtemIndexDeObj/index.jsx
@@ -0,0 +1,14 @@
+const getIndex = () => {
+
+    const keys = Object.keys(playerAnalysis);
+    console.log(keys)
+    if (index >= 0 && index < keys.length) {
+      const key = keys[index];
+      const data = playerAnalysis[key];
+    //   console.log(data);
+
+    }
+
+  
+    
+}
